a65b6d8a2f86464018273cb21b1db298c88718e0
calm <calm@calm.com>
2025-03-06 22:03:24 +0800

    init

--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 
 - 跨链桥 [bridge.md](contract/docs/bridge.md)
 - vote合约 [vote.md](contract/docs/vote.md)
-- 业务合约,ERC20跨链demo [tantin_bridge.md](contract/docs/tantin_bridge.md)
+- 业务合约 [tantin_bridge.md](contract/docs/tantin_bridge.md)
+- 测试网合约信息 [testnet.md](contract/docs/testnet.md)
+- 主网合约信息 [mainnet.md](contract/docs/mainnet.md)
---
+++ contract/contracts/ERC20.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract ERC20Token is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    uint8 public decimal;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 _decimal,
+        address minter
+    ) ERC20(name, symbol) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, minter);
+        decimal = _decimal;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return decimal;
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
+}
