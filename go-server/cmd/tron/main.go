89cfca43771b858a27933a4e1c7398440e37e958
calm <calm@calm.com>
2025-03-24 03:01:09 +0800

    1

--- go-server/cmd/tron/main.go
+++ go-server/cmd/tron/main.go
@@ -1,12 +1,7 @@
 package main
 
 import (
-	"coinstore/contract"
 	"coinstore/services"
-	"fmt"
-	"github.com/fbsobreira/gotron-sdk/pkg/address"
-	"math/big"
-	"strings"
 )
 
 func main() {
@@ -25,19 +20,19 @@ func main() {
 	//}
 	//vote.Init()
 
-	tantin, err := contract.NewTanTinTron()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	//tantin, err := contract.NewTanTinTron()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
 	//tantin.Init()
 
-	//for {
-	toAddress, err := address.Base58ToAddress("TFBymbm7LrbRreGtByMPRD2HUyneKabsqb")
-	fmt.Println(toAddress.String())
-	txHash, err := tantin.Deposit("2", strings.TrimPrefix(contract.ResourceIdUsdt, "0x"), toAddress.String(), "0x00", big.NewInt(1))
-
-	fmt.Println(txHash, err)
+	////for {
+	//toAddress, err := address.Base58ToAddress("TFBymbm7LrbRreGtByMPRD2HUyneKabsqb")
+	//fmt.Println(toAddress.String())
+	//txHash, err := tantin.Deposit("2", strings.TrimPrefix(contract.ResourceIdUsdt, "0x"), toAddress.String(), "0x00", big.NewInt(1))
+	//
+	//fmt.Println(txHash, err)
 	//time.Sleep(time.Second * 30)
 	//}
 
