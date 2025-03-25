22c728870b631312b3b2bd8c351cd11411d3ca4a
calm <calm@calm.com>
2025-03-25 21:25:32 +0800

    1

--- go-server/cmd/server/service/bridge_latest_time.go
+++ go-server/cmd/server/service/bridge_latest_time.go
@@ -1,7 +1,10 @@
 package service
 
 import (
+	"coinstore/db"
+	"coinstore/model"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func BridgeLatestTime(c *gin.Context) {
@@ -15,17 +18,20 @@ func BridgeLatestTime(c *gin.Context) {
 	}
 	chainIdFrom := c.Query("chain_id_from")
 	chainIdTo := c.Query("chain_id_to")
-	if len(chainIdFrom) == 0 || len(chainIdTo) == 0 {
+	var bridgeTx model.BridgeTx
+	if err := db.DB.Model(model.BridgeTx{}).Where("source_chain_id=? and destination_chain_id=? and bridge_status=2", chainIdFrom, chainIdTo).Order("id desc").First(&bridgeTx).Error; err != nil {
 		c.JSON(200, gin.H{
 			"code": 1,
-			"msg":  "Address and address type are required",
+			"msg":  "参数错误",
 		})
 		return
 	}
+	depositAt, _ := time.ParseInLocation("2006-01-02 15:04:05", bridgeTx.DepositAt, time.Local)
+	receiveAt, _ := time.ParseInLocation("2006-01-02 15:04:05", bridgeTx.ReceiveAt, time.Local)
 
 	c.JSON(200, gin.H{
 		"code": 0,
-		"data": 180,
+		"data": receiveAt.Unix() - depositAt.Unix(),
 		"msg":  "OK",
 	})
 }
