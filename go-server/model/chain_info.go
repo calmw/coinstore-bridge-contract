f978a5b1a06ddee809c1ddcc42e0cea8d90ff661
calm <calm@calm.com>
2025-04-01 00:49:08 +0800

    1

--- go-server/model/chain_info.go
+++ go-server/model/chain_info.go
@@ -11,21 +11,21 @@ type ChainInfo struct {
 	ChainName          string          `gorm:"chain_name;comment:'链名称'" json:"chain_name"`
 	Logo               string          `gorm:"chain_name;type:longtext;comment:'链logo'" json:"logo"`
 	ChainId            int             `gorm:"chain_id;comment:'自定义链ID'" json:"chain_id"`
-	ChainType          int             `gorm:"chain_id;comment:'自定义链类型ID,1 EVM 2 TRON'" json:"chain_type"`
-	Endpoint           string          `gorm:"endpoint;comment:'RPC'" json:"endpoint"`
-	From               string          `gorm:"from;comment:'签名账户地址'" json:"from"`
-	PrivateKey         string          `gorm:"private_key;comment:'签名账户私钥'" json:"private_key"`
-	FreshStart         bool            `gorm:"fresh_start;comment:'启动时，是否从设置的起始块高开始.true 从设置的块高开始，false 从block store开始'" json:"fresh_start"`
-	LatestBlock        bool            `gorm:"latest_block;comment:'启动时，是否从设置的起始块高开始.true 从区块链当前的块高开始'" json:"latest_block"`
-	BridgeContract     string          `gorm:"bridge_contract;comment:'bridge合约地址'" json:"bridge_contract"`
-	VoteContract       string          `gorm:"vote_contract;comment:'vote合约地址'" json:"vote_contract"`
-	GasLimit           int64           `gorm:"gas_limit;comment:''" json:"gas_limit"`
-	MaxGasPrice        int64           `gorm:"max_gas_price;comment:''" json:"max_gas_price"`
-	MinGasPrice        int64           `gorm:"min_gas_price;comment:''" json:"min_gas_price"`
-	Http               int             `gorm:"http;default:1;comment:'http or ws. 1 http 0 ws'" json:"http"`
-	StartBlock         decimal.Decimal `gorm:"type:bigint(30);start_block;comment:'起始块高'" json:"start_block"`
-	BlockStore         decimal.Decimal `gorm:"type:bigint(30);block_store;comment:'扫过的块高'" json:"block_store"`
-	BlockConfirmations int64           `gorm:"block_confirmations;comment:'待确认块高差'" json:"block_confirmations"`
+	ChainType          int             `gorm:"chain_id;comment:'自定义链类型ID,1 EVM 2 TRON'" json:"-"`
+	Endpoint           string          `gorm:"endpoint;comment:'RPC'" json:"-"`
+	From               string          `gorm:"from;comment:'签名账户地址'" json:"-"`
+	PrivateKey         string          `gorm:"private_key;comment:'签名账户私钥'" json:"-"`
+	FreshStart         bool            `gorm:"fresh_start;comment:'启动时，是否从设置的起始块高开始.true 从设置的块高开始，false 从block store开始'" json:"-"`
+	LatestBlock        bool            `gorm:"latest_block;comment:'启动时，是否从设置的起始块高开始.true 从区块链当前的块高开始'" json:"-"`
+	BridgeContract     string          `gorm:"bridge_contract;comment:'bridge合约地址'" json:"-"`
+	VoteContract       string          `gorm:"vote_contract;comment:'vote合约地址'" json:"-"`
+	GasLimit           int64           `gorm:"gas_limit;comment:''" json:"-"`
+	MaxGasPrice        int64           `gorm:"max_gas_price;comment:''" json:"-"`
+	MinGasPrice        int64           `gorm:"min_gas_price;comment:''" json:"-"`
+	Http               int             `gorm:"http;default:1;comment:'http or ws. 1 http 0 ws'" json:"-"`
+	StartBlock         decimal.Decimal `gorm:"type:bigint(30);start_block;comment:'起始块高'" json:"-"`
+	BlockStore         decimal.Decimal `gorm:"type:bigint(30);block_store;comment:'扫过的块高'" json:"-"`
+	BlockConfirmations int64           `gorm:"block_confirmations;comment:'待确认块高差'" json:"-"`
 	Open               int64           `gorm:"open;comment:'是否开启 1 开启 0关闭'；default:1" json:"open"`
 }
 
