30cd95dff778a6b097d4135af6f9d7bdc112f181
calm <calm@calm.com>
2025-04-06 08:51:52 +0800

    1

--- go-server/cmd/bsc/main.go
+++ go-server/cmd/bsc/main.go
@@ -3,23 +3,17 @@ package main
 import (
 	"coinstore/contract"
 	"coinstore/services"
-	"coinstore/utils"
 	"fmt"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/status-im/keycard-go/hexutils"
-	"math/big"
-	"strings"
-	"time"
 )
 
 func main() {
 	services.InitBscEnv()
-	bridge, err := contract.NewBridge()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	bridge.Init()
+	//bridge, err := contract.NewBridge()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//bridge.Init()
 	//bridge.AdminSetResource(big.NewInt(1))
 	//
 	//vote, err := contract.NewVote()
@@ -34,19 +28,19 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	//tantin.Init()
+	tantin.Init()
 	//tantin.AdminSetToken()
 
-	resourceIdBytes := hexutils.HexToBytes(strings.TrimPrefix(contract.ResourceIdUsdt, "0x"))
-	//resourceIdBytes := hexutils.HexToBytes(strings.TrimPrefix(contract.ResourceIdCoin, "0x"))
-
-	signature, err := utils.RecipientSignature()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for {
-		tantin.Deposit(common.HexToAddress("0x80B27CDE65Fafb1f048405923fD4a624fEa2d1C6"), [32]byte(resourceIdBytes), big.NewInt(1), big.NewInt(10), signature)
-		time.Sleep(time.Second * 30)
-	}
+	//resourceIdBytes := hexutils.HexToBytes(strings.TrimPrefix(contract.ResourceIdUsdt, "0x"))
+	////resourceIdBytes := hexutils.HexToBytes(strings.TrimPrefix(contract.ResourceIdCoin, "0x"))
+	//
+	//signature, err := utils.RecipientSignature()
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//for {
+	//	tantin.Deposit(common.HexToAddress("0x80B27CDE65Fafb1f048405923fD4a624fEa2d1C6"), [32]byte(resourceIdBytes), big.NewInt(1), big.NewInt(10), signature)
+	//	time.Sleep(time.Second * 30)
+	//}
 }
