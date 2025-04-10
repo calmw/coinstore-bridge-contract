a9d4b43063ed0f82baff3f89435386182188973e
calm <calm@calm.com>
2025-04-10 13:16:48 +0800

    1

--- go-server/cmd/eth/main.go
+++ go-server/cmd/eth/main.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"coinstore/contract"
-	"coinstore/services"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
 )
 
 func main() {
-	services.InitEthEnv()
+	contract.InitEthEnv()
 	bridge, err := contract.NewBridge()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	bridge.Init()
-	//bridge.AdminSetResource(big.NewInt(1))
-	//
+	bridge.AdminSetResource(
+		contract.ResourceIdEth,
+		2,
+		common.HexToAddress(contract.ZeroAddress),
+		big.NewInt(100),
+	)
+	bridge.AdminSetResource(
+		contract.ResourceIdWeth,
+		2,
+		common.HexToAddress(contract.ChainConfig.UsdtAddress),
+		big.NewInt(100),
+	)
+
 	//vote, err := contract.NewVote()
 	//if err != nil {
 	//	fmt.Println(err)
