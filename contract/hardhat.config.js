d1580903bde27820b116774a6d4dd0a6e17bf0ef
calm <calm@calm.com>
2025-04-19 09:40:37 +0800

    1

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -17,6 +17,11 @@ module.exports = {
             accounts: [`${PRIVATE_KEY}`]
             // gasPrice: 10000000000
         },
+        bsc_mainnet: {
+            url: "https://bsc-mainnet.infura.io/v3/59ec080dc74d4af893ea04bfe2b168b5",
+            accounts: [`${PRIVATE_KEY}`]
+            // gasPrice: 10000000000
+        },
         ethereum: {
             // url: "https://mainnet.infura.io/v3/59ec080dc74d4af893ea04bfe2b168b5",
             url: "https://mainnet.infura.io/v3/59ec080dc74d4af893ea04bfe2b168b5",
