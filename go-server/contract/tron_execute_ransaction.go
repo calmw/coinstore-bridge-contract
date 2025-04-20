375551b5988516037b55b6cd83dc21b0680482f7
calm <calm@calm.com>
2025-04-20 20:21:48 +0800

    增加签名机

--- go-server/contract/tron_execute_ransaction.go
+++ go-server/contract/tron_execute_ransaction.go
@@ -12,15 +12,15 @@ func ExecuteTronTransaction(c *transaction.Controller, chainId int, fromAddress,
 	fmt.Println(c.Behavior.SigningImpl, "~~~~~~~~~")
 	switch c.Behavior.SigningImpl {
 	case transaction.Software:
-		fmt.Println("签名1：", fmt.Sprintf("%x", c.Tx.String()))
+		//fmt.Println("签名1：", fmt.Sprintf("%x", c.Tx.String()))
 		fmt.Println("签名1：", fmt.Sprintf("%x", c.Tx.Signature))
 		c.SignTxForSending()
-		//fmt.Println("签名2：", fmt.Sprintf("%x", c.Tx.Signature))
-		err := SignTxForSending(c, chainId, fromAddress, apiSecret)
-		if err != nil {
-			fmt.Println("从签名机获取签名错误", err)
-			return err
-		}
+		fmt.Println("签名2：", fmt.Sprintf("%x", c.Tx.Signature))
+		//err := SignTxForSending(c, chainId, fromAddress, apiSecret)
+		//if err != nil {
+		//	fmt.Println("从签名机获取签名错误", err)
+		//	return err
+		//}
 	case transaction.Ledger:
 		c.HardwareSignTxForSending()
 	}
