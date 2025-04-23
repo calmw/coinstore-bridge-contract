e90106a72ec4db61a16f13f6191caca4c328eb8f
calm <calm@calm.com>
2025-04-23 18:08:19 +0800

    增加签名机

--- go-server/bridge/chains/tron/execute_ransaction.go
+++ go-server/bridge/chains/tron/execute_ransaction.go
@@ -1,20 +1,16 @@
 package tron
 
-import "C"
-import (
-	"fmt"
-	"github.com/fbsobreira/gotron-sdk/pkg/client/transaction"
-)
+//import "C"
 
-func ExecuteTransaction(c *transaction.Controller) error {
-	fmt.Println(c.Behavior.SigningImpl, "~~~~~~~~~")
-	switch c.Behavior.SigningImpl {
-	case transaction.Software:
-		C.signTxForSending()
-	case transaction.Ledger:
-		C.hardwareSignTxForSending()
-	}
-	C.sendSignedTx()
-	C.txConfirmation()
-	return C.executionError
-}
+//func ExecuteTransaction(c *transaction.Controller) error {
+//	fmt.Println(c.Behavior.SigningImpl, "~~~~~~~~~")
+//	switch c.Behavior.SigningImpl {
+//	case transaction.Software:
+//		C.signTxForSending()
+//	case transaction.Ledger:
+//		C.hardwareSignTxForSending()
+//	}
+//	C.sendSignedTx()
+//	C.txConfirmation()
+//	return C.executionError
+//}
